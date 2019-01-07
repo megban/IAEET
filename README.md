04182abd13a2d0c040271db309cfa81f685bd932
Meghna Banerjee <mebanerj@ucsc.edu>
2019-01-07 11:43:33 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IAEET
+# IAEET
---
+++ home.js
@@ -0,0 +1,12 @@
+
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    showCursor: false,
+    typeSpeed: 200,
+    backSpeed: 50,
+    backDelay: 500,
+    startDelay: 50,
+    loop: true
+
+  });
